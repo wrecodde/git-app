6e0014b42f28ef9f974834c4cb78a7f1065ae8d7
Deji Joseph <wrecodde@gmail.com>
2017-12-15 22:16:50 +0100

    @& + url config...

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main import views
 # my app's urls
 
 urlpatterns = [
-	url(r'^', views.index, name='index'),
+	url(r'^$', views.index, name='index'),
 	url(r'^profile', views.profile, name='profile'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, HttpResponse
 
+import json
+
+import requests
+
+
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Xup, Dawg!")
+	return HttpResponse("<h1>Git App [Built with Django]</h1>")
 
 def profile(request):
-	return ("Where'd I put that profile!?")
+	try:
+		request = requests.get('https://api.github.com/users/wrecodde')
+		print('>>> internet')
+		response = json.loads(request.text)
+		print(response)
+	except:
+		request = open('wrecodde.txt', 'r').read()
+		print('>>> local storage')
+		response = json.loads(request)
+		print(response)
+	
+	return HttpResponse(response)
